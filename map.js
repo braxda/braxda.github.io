156c52d608453ecca8fc45bbb2138053a4f9d2b6
braxda <dylanbracken30@gmail.com>
2025-02-04 20:43:12 -0500

    update map.js

--- map.js
+++ map.js
@@ -9,24 +9,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Load player locations
-fetch('geojson/players.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-          radius: 5,
-          fillColor: "red",
-          color: "black",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        });
-      },
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(`<b>Player:</b> ${feature.properties.name}<br>
-                         <b>Position:</b> ${feature.properties.position}<br>
-                         <b>Hometown:</b> ${feature.properties.hometown}`);
-      }
-    }).addTo(map);
-  });
+//    fetch('geojson/players.geojson')
+//      .then(response => response.json())
+//      .then(data => {
+//        L.geoJSON(data, {
+//          pointToLayer: function (feature, latlng) {
+//            return L.circleMarker(latlng, {
+//              radius: 5,
+//              fillColor: "red",
+//              color: "black",
+//              weight: 1,
+//              opacity: 1,
+//              fillOpacity: 0.8
+//            });
+//          },
+//          onEachFeature: function (feature, layer) {
+//            layer.bindPopup(`<b>Player:</b> ${feature.properties.name}<br>
+//                             <b>Position:</b> ${feature.properties.position}<br>
+//                             <b>Hometown:</b> ${feature.properties.hometown}`);
+//          }
+//        }).addTo(map);
+//      });
